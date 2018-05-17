a1b3e6bb9b80f4c0bd4e25336dd32b58dcd2e1f4
keymaker369 <keymaker369@vo15>
2018-05-17 15:53:28 -0700

    fixed security

--- test01/src/main/java/com/example/test01/SpringSecurityConfig.java
+++ test01/src/main/java/com/example/test01/SpringSecurityConfig.java
@@ -1,12 +1,14 @@
 package com.example.test01;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
@@ -19,14 +21,20 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable();
-        http.authorizeRequests().antMatchers("/", "/note/**").permitAll()
+        http.authorizeRequests().antMatchers("/", "/api/**").permitAll()
         .anyRequest().authenticated();
         http.httpBasic().authenticationEntryPoint(authEntryPoint);
 	}
 
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("test01").password("password").roles("USER");
+		auth.inMemoryAuthentication().withUser("test01")
+		.password("$2a$04$AjFEmZeX7mN8zSn57PUEZeJgBeoKMvwteZMBiP57Jb4AGFsUORmLC").roles("USER");//password
+	}
+	
+	@Bean
+	public BCryptPasswordEncoder encoder() {
+	return new BCryptPasswordEncoder();
 	}
 
 }
